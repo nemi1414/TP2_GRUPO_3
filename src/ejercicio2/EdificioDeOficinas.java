d3a2080d71cb177d19850f3a05aec8128d6d5d51
Alejandro-Madero <madero.ale@gmail.com>
2026-04-14 20:03:14 -0300

    Agrego clase EdificioDeOficinas y .gitignore

---
+++ src/ejercicio2/EdificioDeOficinas.java
@@ -0,0 +1,19 @@
+package ejercicio2;
+
+public class EdificioDeOficinas implements IEdificio {
+
+	private int numeroDeOficinas; 
+	
+	//constructores 
+	
+	//getters / setters
+	
+	
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
