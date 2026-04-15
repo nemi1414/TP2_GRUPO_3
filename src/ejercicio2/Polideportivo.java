09a523530d12d3985df8a8ac37dd2022a3e78728
Julian Contreras <jjulian.contrerass@gmail.com>
2026-04-15 15:52:14 -0300

    Agrego la clase Polideportivo

---
+++ src/ejercicio2/Polideportivo.java
@@ -0,0 +1,15 @@
+package ejercicio2;
+
+public class Polideportivo implements IInstalacionDeportiva, IEdificio {
+
+	@Override
+	public int getTipoDeInstalacion() {
+		return 0;
+	}
+
+	@Override
+	public double getSuperficieEdificio() {
+		return 0;
+	}
+
+}
