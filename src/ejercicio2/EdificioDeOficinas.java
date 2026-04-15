824de7440848cd145c69a286ec542a490e9446e7
Matias Zarate <m.zarate92@hotmail.com>
2026-04-14 21:42:03 -0300

    Logica EdificioDeOficinas

--- src/ejercicio2/EdificioDeOficinas.java
+++ src/ejercicio2/EdificioDeOficinas.java
@@ -3,17 +3,38 @@ package ejercicio2;
 public class EdificioDeOficinas implements IEdificio {
 
 	private int numeroDeOficinas; 
+	private double superficie;
 	
 	//constructores 
+	public EdificioDeOficinas(int numeroDeOficinas, double superficie) {
+		this.numeroDeOficinas = numeroDeOficinas;
+		this.superficie = superficie;
+	}
 	
 	//getters / setters
+	public int getNumeroDeOficinas() {
+		return numeroDeOficinas;
+	}
 	
+
+	public void setNumeroDeOficinas(int numeroDeOficinas) {
+		this.numeroDeOficinas = numeroDeOficinas;
+	}
 	
+	public double getSuperficie() {
+		return superficie;
+	}
+	
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
 	@Override
 	public double getSuperficieEdificio() {
-		// TODO Auto-generated method stub
-		return 0;
+		return superficie;
 	}
-
 	
+	@Override
+	public String toString() {
+		return "EdificioDeOficinas [numeroDeOficinas=" + numeroDeOficinas + ", superficie=" + superficie + "]";
+	}	
 }
