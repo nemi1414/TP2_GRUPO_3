157a145e635c4637aee7deac2f00155b6020b629
LucasBerlingeri <lucasberlingeri4@gmail.com>
2026-04-15 21:55:09 -0300

    completo clase polideportivo en ejercicio 2

--- src/ejercicio2/Polideportivo.java
+++ src/ejercicio2/Polideportivo.java
@@ -2,14 +2,59 @@ package ejercicio2;
 
 public class Polideportivo implements IInstalacionDeportiva, IEdificio {
 
+	// Atributos
+	
+	private String nombre;
+	private double superficie;
+	private int tipoDeInstalacion; 
+
+	// Constructor
+	
+	public Polideportivo(String nombre, double superficie, int tipoDeInstalacion) {
+		this.nombre = nombre;
+		this.superficie = superficie;
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+
+	// Metodos de interfaces
+	
 	@Override
 	public int getTipoDeInstalacion() {
-		return 0;
+		return tipoDeInstalacion;
 	}
 
 	@Override
 	public double getSuperficieEdificio() {
-		return 0;
+		return superficie; 
 	}
 
+	//  Getters & Setters 
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+
+	public void setTipoDeInstalacion(int tipoDeInstalacion) {
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+
+	// Metodo toString
+	
+	@Override
+	public String toString() {
+		return "Polideportivo [Nombre=" + nombre + ", Superficie=" + superficie 
+				+ " m2, Tipo de Instalacion=" + tipoDeInstalacion + "]";
+	}
 }
