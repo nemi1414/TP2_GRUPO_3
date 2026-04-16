b3ec8e0eef7ef3c1f8c95b2a6b0785abff58c6f2
Alejandro-Madero <madero.ale@gmail.com>
2026-04-16 20:34:56 -0300

    Agrego método main al ejercicio 2 y el arrayList para la coleccion polimorfica

---
+++ src/ejercicio2/MainEjercicio2.java
@@ -0,0 +1,20 @@
+package ejercicio2;
+import java.util.ArrayList;
+
+public class MainEjercicio2 {
+
+	public static void main(String[] args) {
+				
+		ArrayList<IEdificio> lista = new ArrayList<IEdificio>(); 
+		
+	
+		//Agregar 3 Polideportivos:
+		
+		
+		//Agregar 2 Edificios de oficinas:
+		
+		
+		//Iterador: 
+	}
+
+}
