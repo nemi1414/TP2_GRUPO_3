1528f6d1310c55af4023fde4dd6ce04d5cde38a2
LucasBerlingeri <lucasberlingeri4@gmail.com>
2026-04-17 09:06:57 -0300

    Agrego oficinas al arraylist

--- src/ejercicio2/mainEjercicio2.java
+++ src/ejercicio2/mainEjercicio2.java
@@ -8,7 +8,7 @@ public class mainEjercicio2 {
 		ArrayList<IEdificio> lista = new ArrayList<IEdificio>(); 
 		
 	
-		//Agregar 3 Polideportivos:
+		// Agregar 3 Polideportivos 
 		Polideportivo p1 = new Polideportivo("Poli Norte", 1500.0, 1);
 		Polideportivo p2 = new Polideportivo("Poli Sur", 2000.0, 2);
 		Polideportivo p3 = new Polideportivo("Poli Centro", 1800.0, 1);
@@ -17,10 +17,18 @@ public class mainEjercicio2 {
 		lista.add(p3);
 		
 		
-		//Agregar 2 Edificios de oficinas:
+		// Agregar 2 Edificios de oficinas:
+		EdificioDeOficinas ofi1 = new EdificioDeOficinas(25, 1200.5);
+		EdificioDeOficinas ofi2 = new EdificioDeOficinas(50, 3400.0);
+		
+		lista.add(ofi1);
+		lista.add(ofi2);
+		
+		System.out.println("Carga de datos exitosa. Elementos en la lista: " + lista.size());
 		
 		
-		//Iterador: 
+		// Iterador
+		
 	}
 
 }
